1296b5424a230b0c2f0bc5a89efd3c8437b662e4
kimminsu <xjfcnfw3@naver.com>
2022-10-14 20:25:10 +0900

    코드 수정

--- baekjoon/bruteforce/15655.py
+++ baekjoon/bruteforce/15655.py
@@ -2,6 +2,6 @@ from itertools import combinations
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
-result =(combinations(arr, m))
+result =list(combinations(arr, m))
 for i in result:
     print(*i)
