1944d23be40dba2aa875324b8370ed6ad2fcf9d0
kimminsu <xjfcnfw3@naver.com>
2022-10-17 09:03:31 +0900

    설명 주석 추가

--- study/graph/bfs.py
+++ study/graph/bfs.py
@@ -3,28 +3,32 @@ from collections import deque
 
 def bfs(graph, start, visited):
     queue = deque([start])
+    # 현제 노드 방문 처리
     visited[start] = True
+    # 큐가 빌 때 까지 반복
     while queue:
+        # 큐 앞에 하나의 원소를 뽑아 출력
         v = queue.popleft()
         print(v, end=' ')
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
 
+
 graph = [
-  [],
-  [2, 3, 8],
-  [1, 7],
-  [1, 4, 5],
-  [3, 5],
-  [3, 4],
-  [7],
-  [2, 6, 8],
-  [1, 7]
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
 ]
 
 visited = [False] * 9
-
 
 bfs(graph, 1, visited)
--- study/graph/dfs.py
+++ study/graph/dfs.py
@@ -1,9 +1,11 @@
-def dfs(graph, v, visied):
-    visied[v] = True
+def dfs(graph, v, visited):
+    # 현재 노드 방문 처리
+    visited[v] = True
     print(v, end=' ')
+    # 현제 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
-        if not visied[i]:
-            dfs(graph, i, visied)
+        if not visited[i]:
+            dfs(graph, i, visited)
 
 graph = [
   [],
@@ -18,6 +20,5 @@ graph = [
 ]
 
 visited = [False] * 9
-
 
 dfs(graph, 1, visited)
