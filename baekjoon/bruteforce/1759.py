93e19cdefd9b954c65cd59d23cc5e5470e2772c2
kimminsu <xjfcnfw3@naver.com>
2022-12-13 14:31:39 +0900

    백준 1759번 수정

--- baekjoon/bruteforce/1759.py
+++ baekjoon/bruteforce/1759.py
@@ -2,6 +2,24 @@ from itertools import combinations
 l, c = map(int, input().split())
 arr = list(input().split())
 arr.sort()
-result = combinations(arr, l)
+result = list(combinations(arr, l))
+
+m = {'a', 'e', 'i', 'o', 'u'}
+n = set()
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) not in m:
+        n.add(chr(i))
+
 for i in result:
-    print(''.join(i))
+    a = False
+    b = False
+    t = 0
+    for j in i:
+        if j in m:
+            a = True
+        elif j in n:
+            t += 1
+        if t == 2:
+            b = True
+    if a and b:
+        print(''.join(i))
