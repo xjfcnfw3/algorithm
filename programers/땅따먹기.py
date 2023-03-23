e745c255f3a9a810f8ad8f9f0b56bbc24ebc7d33
kimminsu <xjfcnfw3@naver.com>
2023-03-23 13:08:59 +0900

    프로그래머스 문제 추가

---
+++ programers/땅따먹기.py
@@ -0,0 +1,6 @@
+def solution(land):
+    dp = land
+    for i in range(1, len(land)):
+        for j in range(4):
+            dp[i][j] += max(dp[i - 1][:j] + dp[i - 1][j + 1:])
+    return max(dp[len(dp) - 1])
