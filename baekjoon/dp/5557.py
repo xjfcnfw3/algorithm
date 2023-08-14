dbd3d75e7d922cddadbce647d1abe64d4d8681fb
kimminsu <xjfcnfw3@naver.com>
2023-08-14 19:28:21 +0900

    백준 dp 문제 추가

---
+++ baekjoon/dp/5557.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+num = arr[-1]
+dp = [[0] * 21 for _ in range(n)]
+
+dp[0][arr[0]] = 1
+
+for i in range(1, n - 1):
+    for j in range(0, 21):
+        if dp[i - 1][j]:
+            if j + arr[i] <= 20:
+                dp[i][j + arr[i]] += dp[i - 1][j]
+            if j - arr[i] >= 0:
+                dp[i][j - arr[i]] += dp[i - 1][j]
+
+print(dp[n - 2][num])
