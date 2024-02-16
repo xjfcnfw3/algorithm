077d6ab776e9a724321dcfd66d890dbbc63a502a
kimminsu <xjfcnfw3@naver.com>
2024-02-16 18:50:50 +0900

    백준 그리디 문제 추가

---
+++ baekjoon/greedy/2812.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+numbers = input()
+
+stack = []
+
+for number in numbers:
+    while stack and stack[-1] < number and k > 0:
+        stack.pop()
+        k -= 1
+    stack.append(number)
+if k > 0:
+    print(''.join(stack[:-k]))
+else:
+    print(''.join(stack))
