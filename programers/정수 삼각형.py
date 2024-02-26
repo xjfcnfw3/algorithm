d44cfda069fed08cdc3e79f101eb4330b36ad7de
kimminsu <xjfcnfw3@naver.com>
2024-02-26 19:05:35 +0900

    프로그래머스 문제 추가

---
+++ programers/정수 삼각형.py
@@ -0,0 +1,10 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j, num in enumerate(triangle[i]):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+    return max(triangle[len(triangle) - 1])
