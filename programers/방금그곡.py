c1c246c5e2de3820d7a33461558ec4216431b26d
kimminsu <xjfcnfw3@naver.com>
2024-03-04 19:50:12 +0900

    프로그래머스 문제 추가

---
+++ programers/방금그곡.py
@@ -0,0 +1,36 @@
+
+def check(m, music):
+    return m in music
+
+def get_music(info):
+    exc = {'C#' :'1' ,'D#' :'2', 'F#' :'3', 'G#' :'4', 'A#' :'5', "B#": '6'}
+    for k, v in exc.items():
+        info = info.replace(k, v)
+    return info
+
+def get_music_info(info, music_time):
+    result = get_music(info)
+    if music_time >= len(result):
+        return result * (music_tim e/ /len(result)) + result[:music_tim e %len(result)]
+    else:
+        return result[:music_time]
+
+def get_time(t):
+    return int(t.split(':')[0] ) *60 + int(t.split(':')[1])
+
+
+def get_musicinfo(m, musicinfo):
+    start, end, title, info = musicinfo.split(",")
+    music_time = get_time(end) - get_time(start)
+    music = get_music_info(info, music_time)
+    return [music_time, check(get_music(m), music), title]
+
+def solution(m, musicinfos):
+    answer = ["", 0]
+    for musicinfo in musicinfos:
+        result = get_musicinfo(m, musicinfo)
+        if result[1] and result[0] > answer[1]:
+            answer = [result[2], result[0]]
+    if not answer[0]:
+        return "(None)"
+    return answer[0]
