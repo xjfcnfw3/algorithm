d7d261f4756d24db70f94711cc54baeb68b875be
kimminsu <xjfcnfw3@naver.com>
2024-05-21 19:19:30 +0900

    백준 dp 문제 수정

--- baekjoon/dp/5582.py
+++ baekjoon/dp/5582.py
@@ -5,12 +5,15 @@ b = input().rstrip()
 
 answer = 0
 
-dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+dp = [[0] * len(b) for _ in range(len(a))]
 
-for i in range(1, len(a) + 1):
-    for j in range(1, len(b) + 1):
-        if a[i - 1] == b[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
+for i in range(len(a)):
+    for j in range(len(b)):
+        if a[i] == b[j]:
+            if i == 0 or j == 0:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j-1] + 1
             if answer < dp[i][j]:
                 answer = dp[i][j]
 
