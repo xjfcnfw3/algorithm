6fdcda5163976d2ad6a50376271f4ffc057c0cc5
kimminsu <xjfcnfw3@naver.com>
2024-08-05 21:34:53 +0900

    백준 1747 문제 수정

--- baekjoon/bruteforce/1747.py
+++ baekjoon/bruteforce/1747.py
@@ -1,23 +1,22 @@
-import math
-
 n = int(input())
 result = 0
 
 
 def check(num):
-    for i in range(2, int(num ** (1 / 2))):
+    for i in range(2, int(num**(1/2)) + 1):
         if num % i == 0:
             return False
     return True
 
 
 for i in range(n, 1000001):
-    if i == 0:
+    if i == 1:
         continue
     if str(i) == str(i)[::-1]:
         if check(i):
             result = i
             break
+
 if result == 0:
     result = 1003001
 print(result)
