4938662128f67c346104796447e006abe3da48b8
kimminsu <xjfcnfw3@naver.com>
2025-02-02 14:43:18 +0900

    백준 dp 문제 수정

--- baekjoon/dp/5582.py
+++ baekjoon/dp/5582.py
@@ -10,10 +10,9 @@ dp = [[0] * len(b) for _ in range(len(a))]
 for i in range(len(a)):
     for j in range(len(b)):
         if a[i] == b[j]:
-            if i == 0 or j == 0:
-                dp[i][j] = 1
-            else:
-                dp[i][j] = dp[i-1][j-1] + 1
+            dp[i][j] = 1
+            if i - 1 >= 0 and j - 1 >= 0:
+                dp[i][j] += dp[i - 1][j - 1]
             if answer < dp[i][j]:
                 answer = dp[i][j]
 
