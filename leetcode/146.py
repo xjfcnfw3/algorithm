c3b8da662ba2fbb64b189966ba19dcd378068e2d
kimminsu <xjfcnfw3@naver.com>
2025-04-15 20:29:08 +0900

    leetcode 문제 추가

---
+++ leetcode/146.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = head
+        lst: list = []
+
+        while p:
+            lst.append(p.val)
+            p = p.next
+
+        lst.sort()
+
+        p = head
+        for i in range(len(lst)):
+            p.val = lst[i]
+            p = p.next
+
+        return head
