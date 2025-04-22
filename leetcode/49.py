eee54ef2bb62231bd18b43ac078452d33d84a7b0
kimminsu <xjfcnfw3@naver.com>
2025-04-22 18:10:11 +0900

    leetcode 문제 추가

---
+++ leetcode/49.py
@@ -0,0 +1,9 @@
+import collections
+
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return anagrams.values()
